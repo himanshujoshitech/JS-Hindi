b7ab25f5b6db32d913a162489f099ab13cdea1a2
himanshujoshitech <himanshujoshi.tech@gmail.com>
2025-01-20 10:58:21 +0000

    datatypes

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail = "himanshu@google.com"
+var accountPassword = "123"
+accountCity = "Jaipur"
+
+// accountId = 2 (not allowed)
+
+accountEmail = "hc@gmail.com"
+accountPassword ="4564"
+accountCity = "Haldwani"
+let accountState;
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+console.log(accountId);
---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // we are using nodejs, not browser this alert function use to show in browser
+
+console.log(3+3); // code readibility should be high so write properly
+
+let name = "Himanshu"
+let age = 18
+let isLoggedIn = true
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // output = undefined
+
+console.log(typeof null); // output = object
+
