ad3df94ec130ae127a45053fd798eb9b7920dea7
himanshujoshitech <himanshujoshi.tech@gmail.com>
2025-01-21 07:42:56 +0000

    comparision of js

--- 01_Basics/03_conversionOperations.js
+++ 01_Basics/03_conversionOperations.js
@@ -1,30 +1,80 @@
-let score = "33abc"
+// let score = "33abc"
 
-console.log(typeof score);  //OR
-console.log(typeof(score));
+// console.log(typeof score);  //OR
+// console.log(typeof(score));
 
-let valueInNumber = Number(score)
-console.log(typeof(valueInNumber))
-console.log(valueInNumber);
+// let valueInNumber = Number(score)
+// console.log(typeof(valueInNumber))
+// console.log(valueInNumber);
 
 
-// "33" => 33 (output)
-// "33abc" => NaN
-// true => 1
-// false => 0
+// // "33" => 33 (output)
+// // "33abc" => NaN
+// // true => 1
+// // false => 0
 
-let isLoggedIn = 1
+// let isLoggedIn = 1
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
 
-// 1 => true; 0 => false (output)
-// "" => false
-// "Himanshu" => true
+// // 1 => true; 0 => false (output)
+// // "" => false
+// // "Himanshu" => true
 
-let someNumber = 33
-let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof(stringNumber));
+// let someNumber = 33
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof(stringNumber));
+
+
+//************************OPERATIONS********************************
+
+let value = -3
+let negvalue = -value
+
+// console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2%3);
+// console.log(2/2);
+// console.log(2*2);
+// console.log(2**3); // 2 times ** means 2 raise to the power 3
+
+let str1 = "Himanshu"
+let str2 = " Joshi"
+
+// let str3 = console.log(str1+str2);
+
+// console.log("2" + 1);
+// console.log(2 + "1");
+// console.log("2" + "1");
+// console.log(2 + 1 + "3");
+// console.log("2" + 1);
+// console.log(2 + "1");
+// console.log("2" + "1");
+// console.log(3 + 4 * 5 % 3 );
+
+
+// console.log(+true);
+
+
+
+let x = 3;
+const y = x++;
+// x is 4; y is 3
+
+console.log(x,y);
+
+let a = 3;
+const b = ++a;
+// a is 4; b is 4
+
+console.log(a,b);
+
+
+
+
+
 
 
---
+++ 01_Basics/04_comparision.js
@@ -0,0 +1,28 @@
+// console.log(2<=2);
+
+// console.log(1<=2);
+
+// console.log("2"> 1); // true
+// console.log(2 > "1");  // true
+
+
+// console.log(null==0);
+// console.log(null > 0);
+// console.log(null>=0);
+
+
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+// console.log(undefined == 0);
+
+console.log("2" == 2); // it converts string into a number
+console.log("2" === 2); // it strictly check the data type also so data type is unequal so false output
+
+
+//************Avoid these comparision ******************/
+
+
+
+
+
+
