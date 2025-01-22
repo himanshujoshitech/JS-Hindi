d6ae939430df32384b669f69c0a193ef8823d4ea
himanshujoshitech <himanshujoshi.tech@gmail.com>
2025-01-22 09:21:15 +0000

    math and num

--- 01_Basics/06_numbers_and_maths.js
+++ 01_Basics/06_numbers_and_maths.js
@@ -20,7 +20,27 @@ console.log(hundreds.toLocaleString('en-IN')); //1,00,000 (output)
 /***************************************** Math ***************************************/
 
 
-console.log(Math);
+// console.log(Math);
+// console.log(Math.abs(-4)); // absolute value means only negative value converts into positive value
+
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2)); // 5 (output) mtlb jara bhi 4 se jaada toh 5 ho jaaega kyuki ceiling mtlb top
+// console.log(Math.floor(4.2)); //4 (output) mtlb niche waale value
+// console.log(Math.min(4,5,9,1)); //1 (output) shows the minimum value
+
+console.log(Math.random()); // value will always comes in between 0 and 1
+console.log(Math.floor(Math.random()*10) + 1); // ab minimum value 1 toh aaege kyuki pehle 0.001 bhi aa skte thei aur usme agar *10 krte tb bhi 1 nhi aata isliye + 1 kiya and floor to remove point
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
+
+
+
+
+
 
 
 
